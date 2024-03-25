5b85365bae37a993e6415e92bbf34df792a689e7
BilRos <billyrossowuor@gmail.com>
2024-03-25 21:27:45 +0300

    Got to explore different ways to use functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+//I have used function expression here
+function saturdayFun(activity="roller-skate"){
+return( 'This Saturday, I want to'+' '+ activity +'!' );  
+}
+saturdayFun("bathe my dog");
+
+//I have used function expression here
+function mondayWork (activity1='go to the office'){
+    return( 'This Monday, I will'+' '+ activity1 +'.' ); 
+}
+mondayWork('work from home');
+
+
+const wrapAdjective = function (visualFlair = "*") {
+    return function (adjective = "special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+};
